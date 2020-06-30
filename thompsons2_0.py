f9f5d01f30f0aa5e6fdc302d799f537f6ef0811b
lylekjoseph <56309659+lylekjoseph@users.noreply.github.com>
2020-06-30 14:28:11 +0200

    Add files via upload

--- thompsons2_0.py
+++ thompsons2_0.py
@@ -9,14 +9,23 @@ class SatsaSpider(scrapy.Spider):
     allowed_domains = ['thompsons.co.za']
     start_urls = ['https://www.thompsons.co.za/deals/south-africa-deals']
 
+
     def parse(self, response):
+        base_url = 'https://www.thompsons.co.za'
 
         for t in range(1,120):
             fjson = {}
             fjson["title"] = response.css(".CampaignPackages-items:nth-child("+str(t)+")  .title a::text").extract()
             fjson["valid dates"] = response.css(".CampaignPackages-items:nth-child("+str(t)+")  .valid-dates p::text").extract()
             fjson["price"] = response.css(".CampaignPackages-items:nth-child("+str(t)+")  .price::text").extract()
+            fjson["relative_img_link"]= response.xpath("//div[@class ='image-gradient']/img/@src").extract()[t]
+            fjson["abs"] = base_url + fjson["relative_img_link"]
+            #fjson['image_urls'] = [url_join_imgz(base_url,relative_img_link)] for t in relative_img_link ]
+            #fjson["relative_img_link"] = response.xpath("//div[@class ='image-gradient']/img/@src").extract()[t]
             fjson["tags"] = response.css(".CampaignPackages-items:nth-child("+str(t)+")  .tag-names::text").extract()
+            fjson["img_link2"] = response.css(".CampaignPackages-items:nth-child("+str(t)+") .lazyloaded").extract()
+
+
             print(fjson)
             print("********************************************************************")
             print(t)
